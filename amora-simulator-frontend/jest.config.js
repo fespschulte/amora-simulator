c5864268a1fb65d4aaa0d609b8a3fe2b5a30e1a7
Fernando Pinto <fschultepinto@gmail.com>
2025-05-27 22:29:20 -0300

    :wrench: chore(frontend): add Dockerfile, Jest config, and package files for Next.js setup

---
+++ amora-simulator-frontend/jest.config.js
@@ -0,0 +1,13 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// Export the config using ES module syntax
+export default createJestConfig(customJestConfig);
